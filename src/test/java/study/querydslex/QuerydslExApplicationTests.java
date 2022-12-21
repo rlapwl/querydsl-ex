77d80f58c9dff1c0e83554a054e747dd848074f4
rlapwl <khji1210@naver.com>
2022-12-21 23:28:54 +0900

    init

---
+++ src/test/java/study/querydslex/QuerydslExApplicationTests.java
@@ -0,0 +1,13 @@
+package study.querydslex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
